2e182982fd3e16c6e04241444cd5744d84fb1b0b
root <root@colonney.(none)>
2009-08-14 20:33:46 +0200

    Added installation of the startup script and starting of the service.

--- manifests/classes/client-sources-web-v50.pp
+++ manifests/classes/client-sources-web-v50.pp
@@ -26,11 +26,6 @@ class openerp::client::sources::web::v50 inherits openerp::client::base {
     require => Exec ["upgrade setuptools"]
   }
 
-  exec {"install openerp-web":
-    command  => "cd /srv/openerp/web-client/ && python setup.py install",
-    require  => [ Openerp::Sources["web-client"], Exec["upgrade setuptools"], Exec["install pyprotocols"], Exec["install extremes"] ], 
-  }
-
   file {"/etc/openerp-web.cfg":
     ensure  => present,
     content => template("openerp/openerp-web.cfg-50.erb"),
@@ -40,9 +35,35 @@ class openerp::client::sources::web::v50 inherits openerp::client::base {
     replace => false,
   }
 
-  exec {"fix openerp-web startup script":
-    command  => "sed -i 's/USER=\"terp\"/USER=\"openerp\"/g' /etc/init.d/openerp-web",
-    require  => Exec["install openerp-web"],
+  exec {"install openerp-web":
+    command  => "cd /srv/openerp/web-client/ && python setup.py install",
+    require  => [ Openerp::Sources["web-client"], Exec["upgrade setuptools"], Exec["install pyprotocols"], Exec["install extremes"] ], 
   }
 
+  exec {"openerp-web startup script":
+    command => "cp /srv/openerp/web-client/scripts/openerp-web /etc/init.d/",
+    creates => "/etc/init.d/openerp-web",
+    require => Exec["install openerp-web"],
+  }
+
+  exec {"fix openerp-web startup script":
+    command  => "sed -i 's/USER=\"terp\"/USER=\"openerp\"/g' /etc/init.d/openerp-web",
+    require  => Exec["openerp-web startup script"],
+  }
+
+  exec {"add web-client to startup":
+    command => "update-rc.d openerp-web defaults 99 12",
+    unless  => "test -e /etc/rc2.d/S20openerp-web || test -e /etc/rc2.d/S99openerp-web",
+    require => [ Exec["fix openerp-web startup script"],
+                 File["/etc/openerp-web.cfg"] ],
+    notify  => Service["openerp-web"],
+  }
+
+  service {"openerp-web":
+    ensure  => running,
+    enable  => true,
+    pattern => "openerp-web.cfg",
+    require => [ File["/etc/openerp-web.cfg"],
+                 Exec["fix openerp-web startup script"] ],
+  }
 }

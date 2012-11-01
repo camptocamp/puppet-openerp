0181c48a6fb4c9f5d7da9a717e7d62d9e357605b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-11-01 09:53:31 +0100

    clean unused classes

--- manifests/client/base.pp
+++
@@ -1,9 +0,0 @@
-class openerp::client::base inherits openerp::base {
-  package {[ "python-gtk2",
-             "python-glade2",
-             "python-egenix-mxdatetime",
-             "python-numpy-ext",
-  ]:
-    ensure => present,
-  }
-}

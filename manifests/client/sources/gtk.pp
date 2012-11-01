4c3cfeaa1f73d32fbf0ddf46bf7470bd6a72c282
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-11-01 09:53:31 +0100

    clean unused classes

--- manifests/client/sources/gtk.pp
+++
@@ -1,9 +0,0 @@
-class openerp::client::sources::gtk inherits openerp::client::base {
-  openerp::sources {"client":
-    ensure      => present,
-    url         => "http://bazaar.launchpad.net/%7Eopenerp/openobject-client/5.0/",
-    basedir     => "/srv/openerp/",
-    owner       => "$openerp_source_owner",
-    group       => "$openerp_source_group",
-  }
-}

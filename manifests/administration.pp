770829fb01b9519ca3a395fa49104980f4463a2e
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-11-01 11:10:45 +0100

    administration uses now class variables

--- manifests/administration.pp
+++ manifests/administration.pp
@@ -1,13 +1,15 @@
-class openerp::administration {
+class openerp::administration (
+  $admin='openerp-admin',
+  $command='/etc/init.d/openerp-multi-instances') {
 
-  group { "openerp-admin":
+  group { 'openerp-admin':
     ensure => present,
   }
 
-  sudo::directive { "openerp-administration":
-    ensure => present,
-    content => template("openerp/sudoers.openerp.erb"),
-    require => Group["openerp-admin"],
+  sudo::directive { 'openerp-administration':
+    ensure  => present,
+    content => template('openerp/sudoers.openerp.erb'),
+    require => Group['openerp-admin'],
   }
 
 }

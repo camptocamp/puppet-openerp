e9bea3eb722fbc6e6ba596029b05511f9ba93477
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-11-02 08:19:00 +0100

    class openerp has now variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
-class openerp {
+class openerp($groups=['dialout','postgres']) {
   if $::lsbdistcodename != 'squeeze' {
     fail "Unsupported system ${::lsbdistcodename}"
+  }
+
+  class {
+    'openerp::base': groups => $groups;
   }
   include openerp::server
 }
